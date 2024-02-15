d1996395dedd443095e4adae2ef1dce1cc507bfe
imaryves <99935138+imaryves@users.noreply.github.com>
2024-02-15 15:46:26 -0500

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Welcome to authentication window"
-echo "Enter your name"
-read name
+echo "Enter your username"
+read username
 echo "Enter your password"
 read -s password
 echo "authentication successful"
